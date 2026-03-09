6ef413d2da0c407604b80f1e8a4ccfe6fbdb8306
KwnxpN <tonnum.kawin@gmail.com>
2026-03-09 09:49:29 +0700

    feat: change users base api to http://10.0.2.2:8000/api/users

--- lib/core/network/user_dio.dart
+++ lib/core/network/user_dio.dart
@@ -3,6 +3,6 @@ import 'package:flutter_project/core/network/base_dio_factory.dart';
 
 class UserDio {
   static Dio create() {
-    return BaseDioFactory.create('http://10.0.2.2:9091', attachToken: true);
+    return BaseDioFactory.create('http://10.0.2.2:8000/api/users', attachToken: true);
   }
 }
